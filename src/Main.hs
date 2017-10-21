1f151946c4685ab6db29f83ff0a30c5e2df62b04
Maciej Smolinski <maciej.smolinski@casumo.com>
2017-10-21 10:47:37 +0200

    Examples: Read/write to memcache using Memcache Client.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import qualified Database.Memcache.Client as M
+import qualified Database.Memcache.Client as MemcacheClient
+import qualified Database.Memcache.Types as MemcacheTypes
+import Data.Maybe (Maybe(..))
+
+type MemcacheResult = Maybe (MemcacheTypes.Value, MemcacheTypes.Flags, MemcacheTypes.Version)
+
+printValue :: MemcacheResult -> IO ()
+printValue Nothing = putStrLn "Miss"
+printValue (Just (value, _, _)) = putStrLn $ "Hit: " ++ show value
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  mc <- MemcacheClient.newClient [MemcacheClient.def] MemcacheClient.def
+  -- MemcacheClient.set mc "key" "Hello World" 0 0
+  value <- MemcacheClient.get mc "key"
+  printValue value
