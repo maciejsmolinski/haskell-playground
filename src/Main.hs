18e3c357f9dc22124c85a069a901196e71870ef6
Maciej Smolinski <maciej.smolinski@casumo.com>
2017-10-22 21:44:58 +0200

    MTL: Use reader monad to easily access connection from within the actions.

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,9 @@ module Main where
 import qualified Database.Memcache.Client as MemcacheClient
 import qualified Database.Memcache.Types as MemcacheTypes
 import Data.Maybe (Maybe(..))
+import Data.Function (flip)
+import Control.Monad.Reader (ReaderT, runReaderT, ask)
+import Control.Monad.IO.Class (liftIO)
 
 type MemcacheResult =
   Maybe (MemcacheTypes.Value, MemcacheTypes.Flags, MemcacheTypes.Version)
@@ -12,22 +15,31 @@ connection :: IO MemcacheClient.Client
 connection =
   MemcacheClient.newClient [MemcacheClient.def] MemcacheClient.def
 
-set :: MemcacheClient.Client -> MemcacheTypes.Key -> MemcacheTypes.Value -> IO MemcacheTypes.Version
-set connection key value =
-  MemcacheClient.set connection key value 0 0
+get :: MemcacheTypes.Key -> ReaderT MemcacheClient.Client IO MemcacheResult
+get key = do
+  connection <- ask
+  liftIO $ MemcacheClient.get connection key
 
-get :: MemcacheClient.Client -> MemcacheTypes.Key -> IO MemcacheResult
-get =
-  MemcacheClient.get
+set :: MemcacheTypes.Key -> MemcacheTypes.Value -> ReaderT MemcacheClient.Client IO MemcacheTypes.Version
+set key value = do
+  connection <- ask
+  liftIO $ MemcacheClient.set connection key value 0 0
+
+flush :: ReaderT MemcacheClient.Client IO ()
+flush = do
+  connection <- ask
+  liftIO $ MemcacheClient.flush connection Nothing
 
 main :: IO ()
 main = do
   mc <- connection
 
-  get mc "counter" >>= print
-  set mc "counter" "1"
-  get mc "counter" >>= print
-  set mc "counter" "2"
-  get mc "counter" >>= print
+  flip runReaderT mc $ do
+    flush
+    get "counter" >>= liftIO . print
+    set "counter" "1"
+    get "counter" >>= liftIO . print
+    set "counter" "2"
+    get "counter" >>= liftIO . print
 
   return ()
